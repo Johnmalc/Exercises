c9511eae2cfa6a3b8c524172b8ca9b60319c0a5e
johnmalc <johnmalc@ubuntu>
2012-10-12 16:09:04 +0200

    johnmalc Signed-off-by: johnmalc <johnmalc@ubuntu>

--- src/malcjohn/TypeCasting.java
+++ src/malcjohn/TypeCasting.java
@@ -1,9 +1,11 @@
 package malcjohn;
 
 public class TypeCasting {
-	public static void main(String args) {
+	public static void main(String[] args) {
 		byte a = 1;
+		float b = (int)(a + 1);
 		System.out.println(a);
+		System.out.println(b);
 		
 	}
 }
