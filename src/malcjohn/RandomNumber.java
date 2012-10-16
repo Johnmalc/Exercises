66a321f42bd979b6a7b7a3963e1e71ee4d06922f
johnmalc <johnmalc@ubuntu>
2012-10-16 21:50:40 +0200

    Signed-off-by: johnmalc <johnmalc@ubuntu>

--- src/malcjohn/RandomNumber.java
+++ src/malcjohn/RandomNumber.java
@@ -1,18 +1,28 @@
 package malcjohn;
 
 import java.util.*;
+import java.lang.*;
 
 public class RandomNumber {
 
 	/**
-	 * @param args
+	 * http://stackoverflow.com/questions/363681/generating-random-number-in-a-range-with-java
 	 */
 	public static void main(String[] args) {
 		Random b = new Random();
 		int a = b.nextInt();
 		System.out.println(b);
 		System.out.println(a);
-	
+		
+		
+		
+		// Min + (int)(Math.random() * ((Max - Min) + 1))
+		for (int d = 0; d < 10; d++) {
+			int f = 0 + (int)(Math.random() * ((100 - 0) + 1));
+			System.out.println(f);
+		}
+		// int f = 5 + (int)(Math.random() * ((10 - 5) + 1));
+		System.out.println("");
 	}
 
 }
