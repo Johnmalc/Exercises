7a7301c60862fba0fd975bccecf535f6b23aa86b
Johnmalc <cincenko@seznam.cz>
2012-11-03 15:25:12 +0100

    sdfg

--- src/malcjohn/FakulatatForWhile.java
+++ src/malcjohn/FakulatatForWhile.java
@@ -4,26 +4,41 @@ public class FakulatatForWhile {
 
 	/**
 	 * @param args
+	 * @return 
 	 */
 	public static void main(String[] args) {
-		int fakultat = 1;
-		int zahl = 5;
+//		int fakultat = 1;
+//		int zahl = 5;
+//		
+//		for(int i = 1; i <= zahl; i++) {
+//			fakultat = fakultat * i;
+//			}
+//		System.out.println(fakultat);
 		
-		for(int i = 1; i <= zahl; i++) {
-			fakultat = fakultat * i;
-			}
-		System.out.println(fakultat);
+		fak(5); 
+		System.out.println(fak(5));
 	
-		
-		System.out.println("kjsldfn");
-		int fak = 1;
-		int zah = 5; // eingabe
-		
-		while (zah > 1) {
-	         fak = fak * zah;
-			zah--;
+		// class fakultat
+	}
+	
+	// oder durch return statement
+	public static int fak(int fakultat) {
+		for (int i = fakultat-1; i > 0; i--) {
+			fakultat = fakultat * i;
 		}
-		System.out.println(fak);		
-	}	
-}
+				
+		return fakultat;
+	}
+	
+//		System.out.println("kjsldfn");
+//		int fak = 1;
+//		int zah = 5; // eingabe
+//		
+//		while (zah > 1) {
+//	         fak = fak * zah;
+//			zah--;
+//		}
+//		System.out.println(fak);		
+	}
+
 
