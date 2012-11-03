23e3e33d567c97e3ff404db36af9dabc843aaea7
Johnmalc <cincenko@seznam.cz>
2012-11-03 17:33:18 +0100

    update

--- src/malcjohn/ArrayUmdrehen.java
+++ src/malcjohn/ArrayUmdrehen.java
@@ -1,19 +1,86 @@
 package malcjohn;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
 public class ArrayUmdrehen {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[] word1 = new String[50];	
+		int [] feld;
+		feld = new int [5];
+		feld[0] = 1;
+		feld[1] = 2;
+		feld[2] = 8;
+		feld[3] = 5;
+		feld[4] = 5;
 		
 		
-		for(int d = 0; d < word1.length; d++ ){
-			word1[d] = d+1;
-			System.out.println(word1[d]);
-		}
-		//System.out.println(word1[d]);
-	}
+		int [] feldNeu;
+		feldNeu = new int [5];
+		feldNeu[0] = 5;
+		feldNeu[1] = 5;
+		feldNeu[2] = 8;
+		feldNeu[3] = 2;
+		feldNeu[4] = 1;
+		
+		bubbleSort(feldNeu);
+		 System.out.println("Array After Bubble Sort");
+         for(int i=0; i < feldNeu.length; i++){
+             System.out.print(feldNeu[i] + " ");
+         }
+		
+//		Arrays.sort(feldNeu);
+//        System.out.println(Arrays.toString(feldNeu));
+		
+           // mit for loop            
+//        int i;
+//        for ( i = 0; i < feld.length; i++) {
+//             System.out.print(feld[i]);
+//        }
+// 
 
+   }
+	private static void bubbleSort(int[] feldNeu) {
+        
+        /*
+         * In bubble sort, we basically traverse the array from first
+         * to array_length - 1 position and compare the element with the next one.
+         * Element is swapped with the next element if the next element is smaller.
+         *
+         * Bubble sort steps are as follows.
+         *
+         * 1. Compare array[0] & array[1]
+         * 2. If array[0] < array [1] swap it.
+         * 3. Compare array[1] & array[2]
+         * 4. If array[1] < array[2] swap it.
+         * ...
+         * 5. Compare array[n-1] & array[n]
+         * 6. if [n-1] < array[n] then swap it.
+         *
+         * After this step we will have smallest element at the last index.
+         *
+         * Repeat the same steps for array[1] to array[n-1]
+         *  
+         */
+       
+        int n = feldNeu.length;
+        int temp = 0;
+       
+        for(int i=0; i < n; i++){
+                for(int j=1; j < (n-i); j++){
+                       
+                        if(feldNeu[j-1] < feldNeu[j]){
+                                //swap the elements!
+                                temp = feldNeu[j-1];
+                                feldNeu[j-1] = feldNeu[j];
+                                feldNeu[j] = temp;
+                        }
+                       
+                }
+        }
+		}
 }
--- src/malcjohn/Aufgabe_3MitInput.java
+++ src/malcjohn/Aufgabe_3MitInput.java
@@ -12,19 +12,37 @@ public class Aufgabe_3MitInput {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Size");
         int yourNumbers = scan.nextInt();
-        double[] array = new double[yourNumbers];
-
+        
         System.out.println("Numbers");
+
+        double[] array = new double[yourNumbers];
 
         for(int i = 0; i < array.length; i++) {
             array[i] = scan.nextDouble();    
         }	
         
+       // System.out.println(array);
         // http://leepoint.net/notes-java/data/arrays/arrays-ex-reverse.html
         // reverse order
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
+        // see file array umdrehen
+        BubleSort(array);
 
+        System.out.println(Arrays.toString(array));
+        
+	}
+	private static void BubleSort(double[] array){
+		double n = array.length;
+		int temp;
+		for(int i = 0; i < n; i++){
+			for(int j = 1; j < (n-1); j++) {
+				// <> zmeni druh razeni
+				if (array [j-1] > array[j]){
+					temp = (int) array[j-1];
+					array[j-1] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
 	}
 
 }
