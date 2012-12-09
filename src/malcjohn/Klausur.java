7bc4ee791f3c378d98fbda43f71ef2dc8ae760bf
Johnmalc <cincenko@seznam.cz>
2012-12-09 21:45:37 +0100

    kalusur

---
+++ src/malcjohn/Klausur.java
@@ -0,0 +1,24 @@
+package malcjohn;
+
+public class Klausur {
+
+	public static void main(String[] args) {
+//		int i1;
+//		try {
+//			i1 = Integer.parseInt(args[1]);
+//			System.out.println(i1);
+//
+//		} catch (NumberFormatException e) {
+//			System.out.println("Parameter keine Zahl");
+//		}
+		
+		
+		
+		int minuten = 90;
+		double stunden = minuten / 60.;
+		minuten %= 60;
+		System.out.println(minuten);
+
+	}
+
+}
