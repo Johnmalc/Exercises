7eb64762ff7e1edaef2b6525f76de048bdea1278
johnmalc <cincenko@seznam.cz>
2013-01-06 21:16:56 +0100

    jhg

---
+++ src/klausur/MehrereParameter.java
@@ -0,0 +1,21 @@
+package klausur;
+
+public class MehrereParameter {
+
+	public static void main(String[] args) {
+		System.out.println(ma(60, 50, 050, 50, 60));
+
+	}
+
+	public static int[] ma(int... cisla) {
+		for (int i = 0; i < cisla.length; i++) {
+			System.out.println("fd");
+
+		}
+		return cisla;
+
+	}
+}
+class dksfj{
+	
+}
